a650096e83dcfe1ef2efce439e910c1054585aa4
woooang <101870580+woooang@users.noreply.github.com>
2022-07-18 16:09:39 +0900

    [2022-07-18] 등록,수정,조회 기능 추가

---
+++ src/main/java/woooang/firstspring/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package woooang.firstspring.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+}
---
+++ src/main/java/woooang/firstspring/domain/posts/PostsResponseDto.java
@@ -0,0 +1,18 @@
+package woooang.firstspring.domain.posts;
+
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
